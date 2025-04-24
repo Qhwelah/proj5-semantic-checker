816af825adf8c8aa1251a2d5f73de7a13bf5cf56
Qhwelah <120686368+Qhwelah@users.noreply.github.com>
2025-04-23 20:22:21 -0400

    Uploaded starter files
    uploaded starter files from course website

---
+++ src/Program.java
@@ -0,0 +1,30 @@
+public class Program {
+    public static void main(String[] args) throws Exception
+    {
+        //  java.io.Reader r = new java.io.StringReader
+        //  (""
+        //  +"num main()\n"
+        //  +"{\n"
+        //  +"    num a;\n"
+        //  +"    a <- 1;\n"
+        //  +"    return 0;\n"
+        //  +"}\n"
+        //  );
+
+        //  if(args.length == 0)
+        //  args = new String[]
+        //  {
+        //      "C:\\cmpsc470-minc-SemanticChecker-sample\\minc\\"
+        //      +"succ_01.minc",
+        //  };
+
+        if(args.length <= 0)
+            return;
+        String minicpath = args[0];
+        java.io.Reader r = new java.io.FileReader(minicpath);
+
+        Compiler compiler = new Compiler(r);
+
+        compiler.Parse();
+    }
+}
